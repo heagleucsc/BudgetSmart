959651bc4d4dcae6f900166637394c8b3b7fd11a
Michelle Nguyen <uareelle@gmail.com>
2017-05-24 03:38:56 -0700

    Added top counter and list elements update from form (no SharedPrefs yet)

--- app/src/main/java/com/heather/eagle/budgetsmart/ItemFormActivity.java
+++ app/src/main/java/com/heather/eagle/budgetsmart/ItemFormActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ public class ItemFormActivity extends AppCompatActivity {
 
     static final public String ITEM_NAME_STRING = "string_1";
     static final public String ITEM_COST_STRING = "string_2";
+    private static final String LOG_TAG = "formActivity";
 
     //private List<String> test;
 
@@ -38,19 +40,24 @@ public class ItemFormActivity extends AppCompatActivity {
         // was looking at: http://stackoverflow.com/questions/30072055/add-items-to-listview-from-other-activity
 
         EditText edv1 = (EditText) findViewById(R.id.itemName);
-        String name = edv1.getText().toString();
         EditText edv2 = (EditText) findViewById(R.id.itemCost);
+        String name = edv1.getText().toString();
         String cost = edv2.getText().toString();
+
         SharedPreferences settings = getSharedPreferences(MainActivity.MYPREFS, 0);
         SharedPreferences.Editor editor = settings.edit();
+
         editor.putString(ITEM_NAME_STRING, name);
         editor.putString(ITEM_COST_STRING, cost);
         editor.commit();
 
-
         Toast.makeText(getApplicationContext(),"Item added", Toast.LENGTH_SHORT).show();
+
         // Go back to main activity
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("Name", name);
+        intent.putExtra("Cost", cost);
+        Log.d(LOG_TAG,"Name and cost added:" + name + cost);
         startActivity(intent);
     }
 
--- app/src/main/java/com/heather/eagle/budgetsmart/MainActivity.java
+++ app/src/main/java/com/heather/eagle/budgetsmart/MainActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -21,21 +23,34 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static final String MYPREFS = "myprefs";
-    private static final String LOG_TAG = "budget-main";
+    private static final String LOG_TAG = "budgetSmart";
+    public static TextView budgetCounter;
+    public static String name;
+    public static String cost;     // may want to deal with as double to change counter
 
     AppInfo appInfo;
     ListView lv;
     private MyAdapter aa;
-    private ArrayList<ListElement> itemList;
+    final static ArrayList<ListElement> itemList = new ArrayList<ListElement>();
 
     private class ListElement {
         ListElement() {};
 
-        ListElement(String item) {
-            item = this.item;
+        ListElement(String name, String cost) {
+            this.name = name;
+            this.cost = cost;
         }
 
-        public String item;
+        public String name;
+        public String cost;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCost() {
+            return cost;
+        }
     }
 
     private class MyAdapter extends ArrayAdapter<ListElement> {
@@ -48,16 +63,55 @@ public class MainActivity extends AppCompatActivity {
             resource = _resource;
             context = _context;
         }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout newView;
+
+            ListElement w = getItem(position);
+
+            // Inflate a new view if necessary.
+            if (convertView == null) {
+                newView = new LinearLayout(getContext());
+                LayoutInflater vi = (LayoutInflater)
+                        getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                vi.inflate(resource,  newView, true);
+            } else {
+                newView = (LinearLayout) convertView;
+            }
+
+            // Fills in the view.
+            TextView tn = (TextView) newView.findViewById(R.id.itemName);
+            TextView tc = (TextView) newView.findViewById(R.id.itemCost);
+            tn.setText(w.name);
+            tc.setText(w.cost);
+
+            return newView;
+        }
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        appInfo = AppInfo.getInstance(this);
-        itemList = new ArrayList<ListElement>();
-        aa = new MyAdapter(this, R.layout.list_element, itemList);
+
+        budgetCounter = (TextView) findViewById(R.id.budgetCurrent);
         lv = (ListView) findViewById(R.id.listView);
+
+        final Button saveButton = (Button) findViewById(R.id.addSaveButton);
+
+        //appInfo = AppInfo.getInstance(this);
+        //itemList = new ArrayList<ListElement>();
+        getNewItemData();
+        aa = new MyAdapter(this, R.layout.list_element, itemList);
+        /*Bundle extras = getIntent().getExtras();
+        if (getIntent().getStringExtra("Name") != null && getIntent().getStringExtra("Cost") != null){
+            name = extras.getString("Name");
+            cost = extras.getString("Cost");
+            itemList.add(new ListElement(name, cost));
+            Log.d(LOG_TAG, "onCreate: name and cost added from form activity: " + name + cost);
+        }*/
         lv.setAdapter(aa);
         aa.notifyDataSetChanged();
     }
@@ -70,17 +124,29 @@ public class MainActivity extends AppCompatActivity {
         lv = (ListView) findViewById(R.id.listView);
         String itemText1 = settings.getString(ItemFormActivity.ITEM_NAME_STRING, "");
         String itemText2 = settings.getString(ItemFormActivity.ITEM_COST_STRING, "");
+
     }
 
-    private void getDataFromPref() {
+    public void getNewItemData() {
         //intent.putExtra("new item", );
         //ArrayList<String> test = getIntent().getStringArrayListExtra("test");
-
+        Bundle extras = getIntent().getExtras();
+        if (getIntent().getStringExtra("Name") != null && getIntent().getStringExtra("Cost") != null){
+            name = extras.getString("Name");
+            cost = extras.getString("Cost");
+            itemList.add(new ListElement(name, cost));
+            Log.d(LOG_TAG, "getNewItemData: name and cost added from form activity: " + name + cost);
+            for (ListElement item : itemList){
+                Log.d(LOG_TAG, "item in list: " + item.name + " " + item.cost);
+            }
+        }
     }
 
     public void onClickAdd(View v) {
-        // Intent to go to next activity
+        // Intent to go to form activity
         Intent intent = new Intent(this, ItemFormActivity.class);
         startActivity(intent);
     }
+
+    public void decrementCounter(){}
 }
